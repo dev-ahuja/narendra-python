5ddd47c9361f8e3ed3e0f5009c49b307b272c58a
Devender Ahuja <devender_ahuja@mckinsey.com>
2022-06-10 17:21:38 +0530

    second commit

---
+++ S3.py
@@ -0,0 +1,8 @@
+#this file is about lab tests on AWS S3. 
+
+import boto3
+
+aws_mg_con = boto3.session.Session(profile_name = "py-bot", region_name = "us-east-1")
+s3=aws_mg_con.client("s3")
+
+print(s3.name)
