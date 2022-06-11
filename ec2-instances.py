53821fc9c1042b01487291a7a7edda97de2fc075
Dev Ahuja <87172852+dev-ahuja@users.noreply.github.com>
2022-06-11 08:50:16 +0530

    commit
    commit

--- ec2-instances.py
+++ ec2-instances.py
@@ -1,5 +1,7 @@
+# In this script we are pulling the information related to any Ec2 running in us-east-1. 
 import boto3
 from pprint import pprint
+#pprint module with display the outpu dictionary data in much organized way than normal print statement output. 
 aws_mgm_con = boto3.session.Session(profile_name = "py-bot")
 
 ec2_con_cli = aws_mgm_con.client(service_name="ec2", region_name="us-east-1")
