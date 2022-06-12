7496a32ceb745fdc9aa87b22f0943ff46ddf92a5
Devender Ahuja <devender_ahuja@mckinsey.com>
2022-06-12 12:36:24 +0530

    test

--- ec2-instances.py
+++ ec2-instances.py
@@ -11,4 +11,4 @@ response = ec2_con_cli.describe_instances()['Reservations']
 for each_item in response:
     for each in each_item['Instances']:
         print("The image ID is: {}\n The Instance ID is: {}\n The machine launched on: {}".format(each['ImageId'],each['InstanceId'],each['LaunchTime'].strftime("%d-%m-%y")))
-
+hello
