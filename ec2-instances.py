1efa835e021ef4751338989a7d6599c9769d4776
Devender Ahuja <ahuja.devender@gmail.com>
2022-06-20 21:22:52 +0530

    Adding Event Driven python script

--- ec2-instances.py
+++ ec2-instances.py
@@ -6,9 +6,10 @@ aws_mgm_con = boto3.session.Session(profile_name = "py-bot")
 
 ec2_con_cli = aws_mgm_con.client(service_name="ec2", region_name="us-east-1")
 
+'''
 response = ec2_con_cli.describe_instances()['Reservations']
-
 for each_item in response:
     for each in each_item['Instances']:
-        print("The image ID is: {}\n The Instance ID is: {}\n The machine launched on: {}".format(each['ImageId'],each['InstanceId'],each['LaunchTime'].strftime("%d-%m-%y")))
+        print("The image ID is: {}\n The Instance ID is: {}\n The machine launched on: {}".format(each['ImageId'],each['InstanceId'],each['LaunchTime'].strftime("%d-%m-%Y")))
+'''
 
