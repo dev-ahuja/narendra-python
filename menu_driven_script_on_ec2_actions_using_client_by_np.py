4de7c388c35d974873dd2226350496e28725312c
Devender Ahuja <ahuja.devender@gmail.com>
2022-06-21 09:05:26 +0530

    new files

--- menu_driven_script_on_ec2_actions_using_client_by_np.py
+++ menu_driven_script_on_ec2_actions_using_client_by_np.py
@@ -1,6 +1,6 @@
 import boto3
 import sys
-aws_mag_con=boto3.session.Session(profile_name="ec2_developer")
+aws_mag_con=boto3.session.Session(profile_name="py-bot")
 ec2_con_re=aws_mag_con.resource(service_name="ec2",region_name="us-east-1")
 ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-east-1")
 
